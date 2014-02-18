68672b4b8a71621abb3db685f26aa32bbd4c2e86
kwnovi <k.wnovi@gmail.com>
2014-02-18 16:18:49 +0100

    bla

--- app/router.php
+++ app/router.php
@@ -1,7 +1,7 @@
 <?php
-require_once(__APP_DIR__.'users\route.php');
-require_once(__APP_DIR__.'users\actions.php');
-require_once(__APP_DIR__.'views\home.php');
+require_once(__APP_DIR__.'users'._SL_.'route.php');
+require_once(__APP_DIR__.'users'._SL_.'actions.php');
+require_once(__APP_DIR__.'views'._SL_.'home.php');
 require_once(__APP_DIR__.'view.php');
 
 // singleton
