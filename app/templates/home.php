ce3dcd79418c0ad04c96285bae9c6da5165e0026
QuentinLB <q.lebour@gmail.com>
2014-02-25 18:51:06 +0100

    toto

--- app/templates/home.php
+++ app/templates/home.php
@@ -1,13 +1,7 @@
   <nav id="A1">
 
     <h2><i class="fa fa-bolt"></i></h2>
-    
-    <h5><a href ="#" rel="tooltip" data-original-title="Liste des contacts" onclick="showContact();" >
-       <i class="fa fa-bars"></i> 
-     </a></h5> 
-     <h5><a href ="#" rel="tooltip" data-original-title="Liste des conversations" onclick="showMsg();">
-       <i class="fa fa-comments"></i> 
-     </a></h5> 
+
 
     <h5><a href ="#" rel="tooltip" data-original-title="Paramètres de votre compte">
        <i class="fa fa-cog"></i> 
@@ -50,6 +44,40 @@
       <!-- Fin de la première partie du menu  -->
 
 <div class="content-area" id="B2">
+
+<div id="contact-list"  >
+  
+  <ul>
+    <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
+    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
+    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
+    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
+    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
+        <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
+    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
+    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
+    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
+    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
+    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
+  </ul>
+
+</div>
+
+</div>
+
+
+</aside>
+
+
+
+<aside id="A4">
+      
+<div class="content-area" id="B4">
 <div id="msg-list">
       <a class="corps" href="#">
         <h3> Rachelle Sanschagrin <span class="next"><i class="fa fa-angle-double-right"></i></span></h3>
@@ -134,179 +162,28 @@
         </a>
 </div>
 
-<div id="contact-list"  >
-  
-  <ul>
-    <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
-    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
-        <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
-    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
-  </ul>
 
-</div>
 
 </div>
 
 
 </aside>
 
+
 <div class="corpus">
-  <div class="profil" id="prof">
-    <div class="photo"><img src="<?php echo __WWW_DIR__;?>/img/chat.png" alt="Profil" class="img-circle" /></div>
-    <div class="description">
-    <span class="titre">Rachelle Sanschagrin</span>
-    <p  class="connected"><i class="fa fa-circle"></i> <strong>En ligne </strong></p>
-    </div>
-  
-  </div>
-<!-- end profil -->
 
-<div class="conversation" id="conver">
-  
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
+<h1>Welcome Rachelle Sanschagrin !</h1>
 
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
+<p><strong>Meaningful but nonetheless useless shit here ..</strong>  </p>
 
-<article class="intervenant">
-  <p class="msg">
-   Une entreprise gagne de l’argent en transformant un produit et en se rémunerant sur cette transformation. La création de valeur pour une entreprise est de générer  un profit par le biais de la transformation de produit. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-  
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-<article class="intervenant">
-  <p class="msg">
-   Une entreprise gagne de l’argent en transformant un produit et en se rémunerant sur cette transformation. La création de valeur pour une entreprise est de générer  un profit par le biais de la transformation de produit. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-
-  
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-<article class="intervenant">
-  <p class="msg">
-   Une entreprise gagne de l’argent en transformant un produit et en se rémunerant sur cette transformation. La création de valeur pour une entreprise est de générer  un profit par le biais de la transformation de produit. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-
-  
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-<article class="intervenant">
-  <p class="msg">
-   Une entreprise gagne de l’argent en transformant un produit et en se rémunerant sur cette transformation. La création de valeur pour une entreprise est de générer  un profit par le biais de la transformation de produit. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-   Les contraintes de temps se renforcent et la concurrence pousse l’entreprise à faire plus. L’entreprise est de plus en plus dépendante de son environnement. une réorganisation des méthodes de production et de management associées. 
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-
-<article class="interlocuteur">
-  <p class="msg">
-    Définition : c’est une transformation de ressources dans le but de créer des biens et des services. Pour cela il va falloir coordonner et faire circuler des flux de création de valeurs, tout en assurant la ponctualité des rendez-vous avec le besoin client. 
-
-  </p>
-  <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
-<article class="intervenant">
-  <p class="msg">
- Ok, super cool ! 
-  </p>
-   <p class="datetime"><i class="fa fa-check-square-o"></i> 10:16 - 10.02.2014</p>
-</article>
-
+<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
 
 
 </div>
 
-<div id="chatbar">
-
- <input type="text"  id="chat"class="form-control" placeholder="Tapez votre message et appuyez sur Entrer pour l'envoyer">
-
-</div>
-
-</div>
-  <script type="text/javascript">
-   //chargement des données de l'utilisateur connecté
-   var USER_DATA = <?php echo $user->toJson();?>
-  </script>
+<script type="text/template" id="contact_template">
+  <% if(data.connected){ %>
+  <span class="size-ic connected"><i class="fa fa-circle"></i></span> <%= data.nickname %>
+  <% else %>
+  <span class="size-ic"><i class="fa fa-circle-o"></i></span> <%= data.nickname %>
+</script>
--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,72 @@
 $(function(){
 
+	var Navigation = Backbone.Router.extend({
+		routes: {
+			"contacts": contacts,
+			"messages": messages
+		},
+		contacts: function(){
+			$("#contact-list").show();
+			$("#msg-list").hide();
+		},
+		messages: function(){
+			$("#contact-list").hide();
+			$("#msg-list").show();
+		}
+	});
+
 	var UserModel = Backbone.Model.extend({
 	
-	})
+	});
 
-	var user = new UserModel(USER_DATA)
-	console.debug(user)
+	var user = new UserModel(USER_DATA);
 
 	var UserContacts = Backbone.Collection.extend({
   		model: UserModel,
-  		url: "/besked/user/contacts",
-  		
-	})
+  		url: "/besked/user/contacts"
+	});
 
-	var contacts = new UserContacts()
+	var ContactsCollection = new UserContacts();
 
-	contacts.fetch();
+	var UserView = Backbone.View.extend({
+		tagName: "li",
+		el: $("#contacts-list ul")
+		template: _.template($('#contact_template').html()),
 
-	console.debug(contacts)
-})
+		initialize: function() {
+		    this.listenTo(this.model, "change", this.render);
+		},
+
+		render: function(){
+			this.$el.html(this.template(this.model.attributes));
+    		return this;
+		}
+	});
+
+	var ContactsView = Backbone.View.extend({
+		el: $("#contacts-list"),
+
+		initialize: function() {
+		    this.listenTo(this.collection, "change", this.render);
+		    this.collection.fetch();
+		    var that = this;
+		    this._userViews = [];
+		 
+		    this.collection.each(function(user) {
+		      that._userViews.push(new UserView({
+		        model: user
+		      }));
+		    });
+		},
+
+		render: function(){
+			var that = this;
+		    this.$el.empty();
+			_(this._userViews).each(function(userView) {
+			    that.$el.append(userView.render().el);
+			});
+		}
+	});
+
+	var contacts = new ContactsView({collection: ContactsCollection});
+});
