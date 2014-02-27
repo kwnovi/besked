640308ea5887d0d7b1c26eda7619c943c1c09a57
QuentinLB <q.lebour@gmail.com>
2014-02-27 01:19:20 +0100

    affichage de la liste des contacts ok

--- app/templates/home.php
+++ app/templates/home.php
@@ -48,22 +48,6 @@
 <div id="contact-list"  >
   
   <ul>
-    <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
-    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
-        <li> <span class="size-ic connected"><i class="fa fa-circle"></i></span> Burnell Corbeil</li>
-    <li> <span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle "></i></span> Luce Déziel </li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Christine Melanson </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Burnell Corbeil</li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Calvin Peterson</li>
-    <li><span class="size-ic connected"><i class="fa fa-circle"></i></span> Luce Déziel </li>
-    <li><span class="size-ic"><i class="fa fa-circle-o"></i></span> Christine Melanson </li>
   </ul>
 
 </div>
@@ -184,6 +168,7 @@
 <script type="text/template" id="contact_template">
   <% if(data.connected){ %>
   <span class="size-ic connected"><i class="fa fa-circle"></i></span> <%= data.nickname %>
-  <% else %>
+  <% } else { %>
   <span class="size-ic"><i class="fa fa-circle-o"></i></span> <%= data.nickname %>
+  <% } %>
 </script>
--- www/js/app.js
+++ www/js/app.js
@@ -1,6 +1,6 @@
 $(function(){
 
-	var Navigation = Backbone.Router.extend({
+	/*var Navigation = Backbone.Router.extend({
 		routes: {
 			"contacts": contacts,
 			"messages": messages
@@ -13,24 +13,22 @@ $(function(){
 			$("#contact-list").hide();
 			$("#msg-list").show();
 		}
-	});
+	});*/
 
 	var UserModel = Backbone.Model.extend({
 	
 	});
 
-	var user = new UserModel(USER_DATA);
+	//var user = new UserModel(USER_DATA);
 
-	var UserContacts = Backbone.Collection.extend({
+	var UserCollection = Backbone.Collection.extend({
   		model: UserModel,
   		url: "/besked/user/contacts"
 	});
 
-	var ContactsCollection = new UserContacts();
-
 	var UserView = Backbone.View.extend({
 		tagName: "li",
-		el: $("#contacts-list ul")
+		//el: $("#contacts-list ul"),
 		template: _.template($('#contact_template').html()),
 
 		initialize: function() {
@@ -38,24 +36,22 @@ $(function(){
 		},
 
 		render: function(){
-			this.$el.html(this.template(this.model.attributes));
+			this.$el.html(this.template({data:this.model.attributes}));
     		return this;
 		}
 	});
 
 	var ContactsView = Backbone.View.extend({
-		el: $("#contacts-list"),
+		el: $("#contact-list>ul"),
 
 		initialize: function() {
 		    this.listenTo(this.collection, "change", this.render);
-		    this.collection.fetch();
 		    var that = this;
-		    this._userViews = [];
-		 
+		    this._userViews = []; 
 		    this.collection.each(function(user) {
-		      that._userViews.push(new UserView({
-		        model: user
-		      }));
+			    that._userViews.push(new UserView({
+			    	model: user
+			    }));
 		    });
 		},
 
@@ -63,10 +59,22 @@ $(function(){
 			var that = this;
 		    this.$el.empty();
 			_(this._userViews).each(function(userView) {
-			    that.$el.append(userView.render().el);
+			    $(that.el).append(userView.render().el);
 			});
 		}
 	});
 
-	var contacts = new ContactsView({collection: ContactsCollection});
+	var contacts_view;
+	var contacts_collection = new UserCollection();
+    contacts_collection.fetch({
+    	success: function(){
+    		contacts_view = new ContactsView({collection: contacts_collection});
+    		contacts_view.render();
+    	},
+    	error: function(){
+    		console.log("error");
+    	}
+    });
+	 
+	
 });
